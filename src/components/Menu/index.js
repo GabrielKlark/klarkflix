ff94a2006eb57115fc05880f9bf91235d758eca2
Gabriel Klark <gabrielklarkgk@gmail.com>
2020-07-29 23:25:21 -0300

    First Project Setup

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css'
+//import ButtonLink from './components/ButtonLink';
+import Button from '../Button/index.js';
+
+export default function Menu () {
+    return (
+        <nav className="Menu">
+            <a href='/'>
+            <img className="Logo" src={Logo} alt="Klarkflix logo" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    )
+}
