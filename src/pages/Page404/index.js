c5d8cfb236d9af4d9cc15b1609dac5e4812b430c
Gabriel Klark <gabrielklarkgk@gmail.com>
2020-07-31 09:51:54 -0300

    Setup Aula 2

---
+++ src/pages/Page404/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageDefault from '../../components/PageDefault';
+
+const Page404 = () => (
+    <PageDefault>
+        Página 404
+    </PageDefault>
+);
+
+export default Page404;
