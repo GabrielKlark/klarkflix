ecf5ba3e1110d07e1b640e8d1c75b57aa2bed9f9
Gabriel Klark <gabrielklarkgk@gmail.com>
2020-08-03 10:15:34 -0300

    aula 4

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -75,7 +75,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição"
-          type="????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
